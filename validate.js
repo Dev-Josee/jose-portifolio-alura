144f20e86a8eb2f6d170410c35f2f8d4e0891759
Dev.Josee <dev.j.carloss@gmail.com>
2024-04-19 22:31:39 -0300

    challenger alura para criação de portifolio

---
+++ validate.js
@@ -0,0 +1,10 @@
+//Seu JavaScript de validação aqui
+
+const title = document.querySelector('.title');
+const about = document.querySelector('.about');
+const skills = document.querySelector('.skills');
+const hobbies = documento.querySelector('.hobbies');
+const academic = document.querySelector('.academic');
+const experience = document.querySelector('.experience');
+const formContato = document.querySelector('.formcontato');
+
