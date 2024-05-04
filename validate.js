e0995f80a0468135971ebc31bccc5cb15de10773
Dev.Josee <dev.j.carloss@gmail.com>
2024-05-04 01:09:44 -0300

    adicionado a responsavidade e também efeito scroll com javascript

--- validate.js
+++ validate.js
@@ -1,10 +1,18 @@
 //Seu JavaScript de validação aqui
 
-const title = document.querySelector('.title');
-const about = document.querySelector('.about');
-const skills = document.querySelector('.skills');
-const hobbies = documento.querySelector('.hobbies');
-const academic = document.querySelector('.academic');
-const experience = document.querySelector('.experience');
-const formContato = document.querySelector('.formcontato');
+const oBservador = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        }else {
+            entry.target.classList.remove('show')
+        }
+    })
 
+});
+
+
+
+const elements = document.querySelectorAll('.animate');
+
+elements.forEach( (element) => oBservador.observe(element) );
